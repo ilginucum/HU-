4588533314ade9570703cd3424d5dc6509517b0f
ilginucum <ilginucum@sabanciuniv.edu>
2025-03-20 11:27:52 +0300

    commit1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HusMuhendislik.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
